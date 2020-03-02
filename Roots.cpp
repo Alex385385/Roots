3d83c18ee68638964fc1734920e514c0e6880357
Alejandro Millan <alejandro_millan@ymail.com>
2020-03-02 15:13:13 -0800

    Initial commit

---
+++ Roots.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Alejandro Millan on 3/2/20.
+//
+
+#include "Roots.h"
+
+Roots::Roots() {}
+
+Roots::~Roots() {}
---
+++ Roots.h
@@ -0,0 +1,17 @@
+//
+// Created by Alejandro Millan on 3/2/20.
+//
+
+#ifndef ROOTS_ROOTS_H
+#define ROOTS_ROOTS_H
+
+
+class Roots {
+public:
+    Roots();
+
+    virtual ~Roots();
+};
+
+
+#endif //ROOTS_ROOTS_H
